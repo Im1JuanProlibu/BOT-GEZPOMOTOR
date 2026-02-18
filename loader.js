7e656b8dbab2e82802eff1e60a79e19d5497665c
Juan Alaguna Prolibu <juan.camilo.alaguna@prolibu.com>
2026-02-18 12:37:53 -0500

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,5 +1,4 @@
-// Solo ejecutar si NO estamos en el editor de Prolibu
-if (window.location.href.indexOf('/app/automation/proposalbot/edit/') === -1) {
+if (window.location.href.indexOf('/edit/') === -1) {
   fetch('https://raw.githubusercontent.com/Im1JuanProlibu/BOT-GEZPOMOTOR/main/index.html?t=' + Date.now())
     .then(function(r) { return r.text(); })
     .then(function(html) {
