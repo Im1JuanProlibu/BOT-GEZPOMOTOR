2308dbd183f8174e9729096a739bc994026300a4
Juan Alaguna Prolibu <juan.camilo.alaguna@prolibu.com>
2026-02-18 12:39:46 -0500

    Change fetch execution to DOMContentLoaded event

--- loader.js
+++ loader.js
@@ -1,9 +1,11 @@
 if (window.location.href.indexOf('/edit/') === -1) {
-  fetch('https://raw.githubusercontent.com/Im1JuanProlibu/BOT-GEZPOMOTOR/main/index.html?t=' + Date.now())
-    .then(function(r) { return r.text(); })
-    .then(function(html) {
-      document.open();
-      document.write(html);
-      document.close();
-    });
+  document.addEventListener('DOMContentLoaded', function() {
+    fetch('https://raw.githubusercontent.com/Im1JuanProlibu/BOT-GEZPOMOTOR/main/index.html?t=' + Date.now())
+      .then(function(r) { return r.text(); })
+      .then(function(html) {
+        document.open();
+        document.write(html);
+        document.close();
+      });
+  });
 }
