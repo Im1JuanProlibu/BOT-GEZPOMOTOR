33d658d39f14c8cf75bd12ea0a3ce6eb4decdc60
Juan Alaguna Prolibu <juan.camilo.alaguna@prolibu.com>
2026-02-18 13:05:30 -0500

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,11 +1,14 @@
-if (window.location.href.indexOf('/edit/') === -1) {
-  setTimeout(function() {
-    fetch('https://raw.githubusercontent.com/Im1JuanProlibu/BOT-GEZPOMOTOR/main/index.html?t=' + Date.now())
-      .then(function(r) { return r.text(); })
-      .then(function(html) {
-        var div = document.createElement('div');
-        div.innerHTML = html;
-        document.body.appendChild(div);
-      });
-  }, 60000);
+function cargarCotizador() {
+  var html = `
+    <div id="chatbot-automotriz">
+      <!-- todo tu HTML del cotizador aquí -->
+    </div>
+  `;
+  
+  var div = document.createElement('div');
+  div.innerHTML = html;
+  document.body.appendChild(div);
 }
+
+// Se ejecuta automáticamente al cargar
+cargarCotizador();
