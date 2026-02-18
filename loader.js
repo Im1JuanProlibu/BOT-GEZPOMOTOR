512f236507973eeb1a6f740abd1c6b35920d34ba
Juan Alaguna Prolibu <juan.camilo.alaguna@prolibu.com>
2026-02-18 11:57:41 -0500

    Add check to prevent execution in Prolibu editor

--- loader.js
+++ loader.js
@@ -1,7 +1,10 @@
-fetch('https://raw.githubusercontent.com/Im1JuanProlibu/BOT-GEZPOMOTOR/main/index.html?t=' + Date.now())
-  .then(function(r) { return r.text(); })
-  .then(function(html) {
-    document.open();
-    document.write(html);
-    document.close();
-  });
+// Solo ejecutar si NO estamos en el editor de Prolibu
+if (window.location.href.indexOf('/app/automation/proposalbot/edit/') === -1) {
+  fetch('https://raw.githubusercontent.com/Im1JuanProlibu/BOT-GEZPOMOTOR/main/index.html?t=' + Date.now())
+    .then(function(r) { return r.text(); })
+    .then(function(html) {
+      document.open();
+      document.write(html);
+      document.close();
+    });
+}
