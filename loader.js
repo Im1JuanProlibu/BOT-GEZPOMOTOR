76f74c08acd8a3d46f2809f0bc87e32570957829
Juan Alaguna Prolibu <juan.camilo.alaguna@prolibu.com>
2026-02-18 11:55:01 -0500

    Load HTML content dynamically from a URL

---
+++ loader.js
@@ -0,0 +1,7 @@
+fetch('https://raw.githubusercontent.com/Im1JuanProlibu/BOT-GEZPOMOTOR/main/index.html?t=' + Date.now())
+  .then(function(r) { return r.text(); })
+  .then(function(html) {
+    document.open();
+    document.write(html);
+    document.close();
+  });
