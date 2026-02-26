5b971cd350e3f30b1fe3d43ae732003e445b6628
Juan Alaguna Prolibu <juan.camilo.alaguna@prolibu.com>
2026-02-25 22:06:46 -0500

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,10 +1,33 @@
-function cargarCotizador() {
-  var html = `
-    <!-- tu HTML aquí -->
+(function() {
+  var GITHUB_URL = 'https://raw.githubusercontent.com/Im1JuanProlibu/BOT-GEZPOMOTOR/main/voyah-bot.html?t=' + Date.now();
 
-    <p>esto es una prueba</p>
-  `;
-  document.body.insertAdjacentHTML('beforeend', html);
-}
+  function loadScript(src, cb) {
+    var s = document.createElement('script');
+    s.src = src; s.onload = cb;
+    s.onerror = function() { cb(); };
+    document.body.appendChild(s);
+  }
 
-cargarCotizador();
+  function loadInOrder(srcs, cb) {
+    if (!srcs.length) return cb();
+    loadScript(srcs[0], function() { loadInOrder(srcs.slice(1), cb); });
+  }
+
+  fetch(GITHUB_URL)
+    .then(function(r) { return r.text(); })
+    .then(function(html) {
+      var tmp = document.createElement('div');
+      tmp.innerHTML = html;
+      var srcs = [], code = '';
+      tmp.querySelectorAll('script').forEach(function(s) {
+        if (s.src) { srcs.push(s.src); s.remove(); }
+        else { code += s.textContent; s.remove(); }
+      });
+      document.getElementById('bot-gezpomotor').innerHTML = tmp.innerHTML;
+      loadInOrder(srcs, function() {
+        var sc = document.createElement('script');
+        sc.textContent = code;
+        document.body.appendChild(sc);
+      });
+    });
+})();
